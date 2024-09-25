16d0d1a3a5b2e65f02d1d2f868f7fddf8821fcae
Nicolas Leotta <nicolea_98@live.com.ar>
2024-09-24 22:37:07 -0300

    Commit clase 24.09, persistencia y conexion WS

---
+++ TemplateTPIntegrador/Negocio/LoginNegocio.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Persistencia;
+
+namespace Negocio
+{
+    public class LoginNegocio
+    {
+        public string Login(string username, string password)
+        {
+            string idUsuario;
+            
+            LoginWS loginWs = new LoginWS();
+            idUsuario = loginWs.login(username, password);
+            Console.WriteLine(idUsuario);
+
+            return idUsuario;
+        }
+    }
+}
--- TemplateTPIntegrador/Persistencia/Class1.cs
+++ TemplateTPIntegrador/Persistencia/Class1.cs
@@ -4,12 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace cai_grupo1
+namespace Persistencia
 {
-    internal class Program
+    public class Class1
     {
-        static void Main(string[] args)
-        {
-        }
     }
 }
