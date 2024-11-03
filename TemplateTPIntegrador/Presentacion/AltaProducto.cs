cf213516e28c26bd969aa200e2c2cc2cec03be31
Karen Antonella Defrance <antonella.defrance@gmail.com>
2024-11-03 19:42:14 -0300

    Carga de Alta de Producto en Form

--- TemplateTPIntegrador/Presentacion/AltaProducto.cs
+++ TemplateTPIntegrador/Presentacion/AltaProducto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Datos;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,26 @@ namespace Presentacion
 {
     public partial class AltaProducto : Form
     {
+        
+
         public AltaProducto()
         {
             InitializeComponent();
+        }
+
+        private void btnGuardarProducto_Click(object sender, EventArgs e)
+        {
+
+            int idcategoria = int.Parse(txtBoxIDCategoria.Text);
+            Guid idusuario = Guid.Parse(txtBoxIDUsuario.Text);
+            Guid idproveedor = Guid.Parse(txtBoxIDProveedor.Text);
+            string nombre = txtBoxNombre.Text;
+            double precio = double.Parse(txtBoxPrecio.Text);
+            int stock = int.Parse(txtBoxStock.Text);   
+
+            Producto producto = new Producto(idcategoria, idusuario, idproveedor, nombre, precio, stock);
+
+
         }
     }
 }
