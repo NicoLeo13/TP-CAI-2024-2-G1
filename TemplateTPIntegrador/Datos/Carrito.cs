c1e0a56d63d26251e02ff16414f7203f8e313d35
Nicolas <nicolea_98@live.com.ar>
2024-11-14 13:55:03 -0300

    WIP - Carrito de Venta

---
+++ TemplateTPIntegrador/Datos/Carrito.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class Carrito
+    {
+        public List<Producto> Productos { get; set; }
+
+        public Carrito()
+        {
+            Productos = new List<Producto>();
+        }
+    }
+}
---
+++ TemplateTPIntegrador/Persistencia/CarritoManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Datos;
+
+namespace Persistencia
+{
+    public class CarritoManager
+    {
+        private Carrito carrito;
+
+        public CarritoManager() 
+        {
+            Carrito carrito = new Carrito();
+        }
+
+        public void AgregarProducto(Producto producto)
+        {
+            carrito.Productos.Add(producto);
+        }
+    }
+}
